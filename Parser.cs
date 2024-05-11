317dfd38d4b3606b3236fb5e5a289076172f0302
NonExistPlayer <ilya08game@gmail.com>
2024-05-11 11:59:47 +0300

    Add source code

---
+++ Parser.cs
@@ -0,0 +1,44 @@
+﻿using System.Xml.Linq;
+using static UFT.Program;
+
+namespace UFT;
+
+#pragma warning disable CS8602
+
+public static class UFTHost
+{
+    public static void CreateFile(string PathToTemplate, string PathToNewFile)
+    {
+        if (!PathToTemplate.Contains('.')) PathToTemplate += ".uft";
+        if (!File.Exists(PathToTemplate)) Error("file does not exist.");
+        string[] file = File.ReadAllLines(PathToTemplate);
+        for (int i = 0; i < file.Length; i++)
+        {
+            file[i] = file[i].Replace("<name>", Path.GetFileNameWithoutExtension(PathToNewFile));
+            file[i] = file[i].Replace("<path>", Path.GetFullPath(PathToNewFile));
+        }
+        XDocument doc = XDocument.Parse(string.Join('\n', file));
+        XElement? output = doc.Root;
+        if (output == null) Error("bad uft. (element: output)");
+        string? newName = null;
+        if (output.Attribute("name") != null)
+            newName = output.Attribute("name").Value;
+        XAttribute? output_ext = output.Attribute("extension");
+        if (output_ext == null) Error("bad uft. (attribute: output extension)");
+
+        File.WriteAllLines(
+            (newName == null ? PathToNewFile : Path.GetDirectoryName(PathToNewFile) + newName) + output_ext.Value,
+            output.Value.Split('\n')[1..
+            (output.Value.Split('\n').Length - (string.IsNullOrWhiteSpace(output.Value) ? 0 : 1))
+            ]
+        );
+    }
+    public static void CreateTemplate(string PathToTemplate)
+    {
+        File.WriteAllText(PathToTemplate, @"<output extension="".txt"">
+Name of file: <name>
+Path to file: <path>
+</output>
+");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace UFT;
+
+internal static class Program
+{
+    private const string Version = "1.0";
+    private static void Main(string[] Args)
+    {
+        if (Args.Length != 2 && Args.Length != 1) Error("requires 1 or 2 arguments");
+        if (Args.Length == 2)
+            UFTHost.CreateFile(Args[0], Args[1]);
+        else if (Args[0] != "about")
+            UFTHost.CreateTemplate(Args[0]);
+        else
+        {
+            Console.WriteLine("UFT - Universal File Template");
+            Console.WriteLine($"Version: {Version}");
+            Console.WriteLine("by NonExistPlayer / nonex");
+        }
+    }
+
+    public static void Error(string Message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("error: "+Message);
+        Console.ResetColor();
+        Environment.Exit(1);
+    }
+}
